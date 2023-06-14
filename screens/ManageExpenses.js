169467fe3f7da2b4cd2979948e64f9961adcdbe2
eladjmc <eladjmc88@gmail.com>
2023-06-14 11:33:06 +0300

    changed the manage expense screen

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,5 +1,8 @@
 import { useLayoutEffect } from "react";
+import { StyleSheet, View } from "react-native";
 import { Text } from "react-native";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/style";
 
 const ManageExpenses = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
@@ -10,7 +13,38 @@ const ManageExpenses = ({ route, navigation }) => {
       title: isEditing ? "Edit Expense" : "Add Expense",
     });
   }, [navigation, isEditing]);
-  return <Text>Manage Expenses Screen</Text>;
+
+  const deleteExpenseHandler = () => {};
+
+  return (
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 };
 
 export default ManageExpenses;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    padding: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
